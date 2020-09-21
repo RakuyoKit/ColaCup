d49b00675df640cfd1ffd9c11f07d9fa251f6b77
Rakuyo <rakuyo.mo@gmail.com>
2020-09-21 16:20:18 +0800

    Create CokeCupController

---
+++ CokeCup/CokeCup/Core/CokeCupController.swift
@@ -0,0 +1,15 @@
+//
+//  CokeCupController.swift
+//  CokeCup
+//
+//  Created by MBCore on 2020/9/21.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// A controller for viewing logs
+open class CokeCupController: UIViewController {
+    
+    
+}
