f35ebfcaafab4b15c1928ab60ec878b98c9984fd
Rakuyo <rakuyo.mo@gmail.com>
2020-09-22 17:02:06 +0800

    Create ColaCupCategoryBar

---
+++ ColaCup/ColaCup/Core/ColaCup/ColaCupCategoryBar.swift
@@ -0,0 +1,15 @@
+//
+//  ColaCupCategoryBar.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2020/9/22.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// View to display the category of logs and the category of modules
+open class ColaCupCategoryBar: UIView {
+    
+    
+}
