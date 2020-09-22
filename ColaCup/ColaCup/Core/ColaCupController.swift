e04a59e9db71f7eee5085aa66f2db768fdf95b98
Rakuyo <rakuyo.mo@gmail.com>
2020-09-22 13:21:09 +0800

    Solve the problem of hidden navigation bar

--- ColaCup/ColaCup/Core/ColaCupController.swift
+++ ColaCup/ColaCup/Core/ColaCupController.swift
@@ -13,3 +13,30 @@ open class ColaCupController: UIViewController {
     
     
 }
+
+// MARK: - Life cycle
+
+extension ColaCupController {
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Solve the problem of hidden navigation bar
+        navigationController?.delegate = self
+        
+        view.backgroundColor = UIColor(red:0.98, green:0.95, blue:0.93, alpha:1.00)
+        
+        
+    }
+}
+
+// MARK: - UINavigationControllerDelegate
+
+extension ColaCupController: UINavigationControllerDelegate {
+    
+    public func navigationController(_ navigationController: UINavigationController, willShow viewController: UIViewController, animated: Bool) {
+        
+        let isHide = viewController is ColaCupController
+        navigationController.setNavigationBarHidden(isHide, animated: animated)
+    }
+}
