8620f0a4aa5a93506ac098745b260c1190e9471e
Rakuyo <rakuyo.mo@gmail.com>
2020-09-22 16:39:55 +0800

    Add headerView related content

--- ColaCup/ColaCup/Core/ColaCup/ColaCupController.swift
+++ ColaCup/ColaCup/Core/ColaCup/ColaCupController.swift
@@ -11,7 +11,43 @@ import UIKit
 /// A controller for viewing logs
 open class ColaCupController: UIViewController {
     
+    /// Top view of the page. Include search box and label selection view.
+    open lazy var headerView: UIView = {
+        
+        let view = UIView()
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10
+        view.backgroundColor = .white
+        
+        return view
+    }()
     
+    /// Used to select a date to view the log of the selected date.
+    open lazy var datePicker: UIDatePicker = {
+        
+        let datePicker = UIDatePicker()
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        datePicker.backgroundColor = .clear
+        
+        datePicker.maximumDate = Date()
+        datePicker.preferredDatePickerStyle = .compact
+        datePicker.datePickerMode = .date
+        
+        return datePicker
+    }()
+    
+    /// Used to search logs.
+    open lazy var searchBar: ColaCupSearchBar = {
+        
+        let searchBar = ColaCupSearchBar()
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.headerView = datePicker
+        
+        return searchBar
+    }()
 }
 
 // MARK: - Life cycle
@@ -26,7 +62,42 @@ extension ColaCupController {
         
         view.backgroundColor = UIColor(red:0.98, green:0.95, blue:0.93, alpha:1.00)
         
+        addSubviews()
         
+        addInitialLayout()
+    }
+}
+
+// MARK: - Config
+
+private extension ColaCupController {
+    
+    func addSubviews() {
+        
+        view.addSubview(headerView)
+        
+        headerView.addSubview(searchBar)
+    }
+    
+    func addInitialLayout() {
+        
+        // headerView
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 12),
+            headerView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10),
+            headerView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -10)
+        ])
+        
+        headerView.setContentHuggingPriority(.required, for: .vertical)
+        
+        // searchBar
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: headerView.topAnchor, constant: 2),
+            searchBar.leftAnchor.constraint(equalTo: headerView.leftAnchor, constant: 10),
+            searchBar.rightAnchor.constraint(equalTo: headerView.rightAnchor, constant: -1)
+        ])
+        
+        searchBar.setContentHuggingPriority(.required, for: .vertical)
     }
 }
 
