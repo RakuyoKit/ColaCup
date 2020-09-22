06f9b863bf91eead890da9d8ca3939c7b4f958a3
Rakuyo <rakuyo.mo@gmail.com>
2020-09-22 13:24:24 +0800

    Add ColaCupSearchBar

---
+++ ColaCup/ColaCup/Core/ColaCupSearchBar.swift
@@ -0,0 +1,15 @@
+//
+//  ColaCupSearchBar.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2020/9/22.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// Search bar in `ColaCupController`
+open class ColaCupSearchBar: UIView {
+    
+    
+}
