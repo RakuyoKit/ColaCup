46f1678ce46c255163156348a2cb6df1cc3737bf
Rakuyo <rakuyo.mo@gmail.com>
2020-09-22 13:17:05 +0800

    Create DetailsViewController

---
+++ ColaCup/ColaCup/Core/DetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DetailsViewController.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2020/9/22.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// Controller that displays the details of the log
+open class DetailsViewController: UIViewController {
+    
+    
+}
+
+// MARK: - Life cycle
+
+extension DetailsViewController {
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor(red:0.98, green:0.95, blue:0.93, alpha:1.00)
+        
+        
+    }
+}
