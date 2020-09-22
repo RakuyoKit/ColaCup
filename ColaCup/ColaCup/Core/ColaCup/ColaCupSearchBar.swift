a46ee7b76add0986bf47d30458e56df9a32fcfc7
Rakuyo <rakuyo.mo@gmail.com>
2020-09-22 16:04:17 +0800

    Improve the content in ColaCupSearchBar

--- ColaCup/ColaCup/Core/ColaCup/ColaCupSearchBar.swift
+++ ColaCup/ColaCup/Core/ColaCup/ColaCupSearchBar.swift
@@ -11,5 +11,97 @@ import UIKit
 /// Search bar in `ColaCupController`
 open class ColaCupSearchBar: UIView {
     
+    public init() {
+        super.init(frame: .zero)
+        config()
+    }
     
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        config()
+    }
+    
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        config()
+    }
+    
+    /// The parent view of all the views below. Responsible for layout constraints.
+    open lazy var stackView: UIStackView = {
+        
+        let stackView = UIStackView()
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 5
+        
+        return stackView
+    }()
+    
+    /// Custom view displayed on the left side of the search box.
+    open lazy var headerView: UIView? = nil {
+        didSet {
+            guard let view = headerView else { return }
+            
+            view.translatesAutoresizingMaskIntoConstraints = false
+            
+            stackView.insertArrangedSubview(splitLine, at: 0)
+            stackView.insertArrangedSubview(view, at: 0)
+        }
+    }
+    
+    /// The dividing line between `headerView` and `searchBar`.
+    /// Only displayed when `headerView` is not `nil`.
+    open lazy var splitLine: UIView = {
+        
+        let line = UIView()
+        
+        line.translatesAutoresizingMaskIntoConstraints = false
+        
+        return line
+    }()
+    
+    /// Search bar.
+    open lazy var searchBar: UISearchBar = {
+        
+        let searchBar = UISearchBar()
+        
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        searchBar.backgroundImage = UIImage()
+        
+        return searchBar
+    }()
+}
+
+// MARK: - Config
+
+private extension ColaCupSearchBar {
+    
+    func config() {
+        
+        addSubviews()
+        addInitialLayout()
+    }
+    
+    func addSubviews() {
+        
+        addSubview(stackView)
+        stackView.addArrangedSubview(searchBar)
+    }
+    
+    func addInitialLayout() {
+        
+        NSLayoutConstraint.activate([
+            stackView.leftAnchor.constraint(equalTo: leftAnchor),
+            stackView.rightAnchor.constraint(equalTo: rightAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+        ])
+        
+        
+    }
 }
