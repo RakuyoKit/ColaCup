2a476cbd5102e2ee1b52a990e6f2aa43578baa71
Rakuyo <rakuyo.mo@gmail.com>
2020-09-23 10:40:21 +0800

    Add test data

--- ColaCup/ColaCup/Other/ViewController.swift
+++ ColaCup/ColaCup/Other/ViewController.swift
@@ -12,9 +12,26 @@ import RaLog
 
 class ViewController: UIViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Log.debug("debug log")
+        Log.warning("warning log")
+        Log.success("success log")
+        Log.error("error log")
+        Log.javascript("javascript log")
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
+        Log.appear(self)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        Log.disappear(self)
     }
     
     @IBAction func enterColaCup(_ sender: Any) {
