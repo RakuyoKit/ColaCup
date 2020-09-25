9b6bf10ce7fe0f7d36eaaac97864f1f1efdfbdea
Rakuyo <rakuyo.mo@gmail.com>
2020-09-25 13:12:21 +0800

    Create DetailsFunctionCell

---
+++ ColaCup/ColaCup/Core/Details/DetailsFunctionCell.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsFunctionCell.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2020/9/25.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// Used in the detail page, the cell that displays the function content
+open class DetailsFunctionCell: UITableViewCell {
+    
+    
+}
