15399a5fbf4dd90050c4b87c1742464f80991993
Rakuyo <rakuyo.mo@gmail.com>
2020-09-25 13:11:46 +0800

    Create DetailsPositionCell

---
+++ ColaCup/ColaCup/Core/Details/DetailsPositionCell.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsPositionCell.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2020/9/25.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// Used in the detail page, the cell that displays the position content
+open class DetailsPositionCell: UITableViewCell {
+    
+    
+}
