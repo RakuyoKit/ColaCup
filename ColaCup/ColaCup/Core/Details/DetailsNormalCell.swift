a5f70a1c7403fab90e8090d66745f840f8a67fc1
Rakuyo <rakuyo.mo@gmail.com>
2020-09-25 13:13:11 +0800

    Create DetailsNormalCell

---
+++ ColaCup/ColaCup/Core/Details/DetailsNormalCell.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsNormalCell.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2020/9/25.
+//  Copyright © 2020 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// Used in the detail page, the cell that displays general content
+open class DetailsNormalCell: UITableViewCell {
+    
+    
+}
