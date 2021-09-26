3a9b287bfbe0882ac435f9352a6f28c91d741912
Rakuyo <rakuyo.mo@gmail.com>
2021-09-26 13:13:00 +0800

    Create FilterViewModel

---
+++ ColaCup/ColaCup/Core/Filter/FilterViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  FilterViewModel.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2021/9/26.
+//  Copyright © 2021 Rakuyo. All rights reserved.
+//
+
+import Foundation
+
+/// Used to handle filtering logic
+open class FilterViewModel {
+    
+    
+}
