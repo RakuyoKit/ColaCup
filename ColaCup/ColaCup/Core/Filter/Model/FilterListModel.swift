6f39beeb96048313093a8e98de13149f26542873
Rakuyo <rakuyo.mo@gmail.com>
2021-09-26 15:38:06 +0800

    Create FilterListModel

---
+++ ColaCup/ColaCup/Core/Filter/Model/FilterListModel.swift
@@ -0,0 +1,15 @@
+//
+//  FilterListModel.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2021/9/26.
+//  Copyright © 2021 Rakuyo. All rights reserved.
+//
+
+import Foundation
+
+/// The model used to configure the filter list.
+public struct FilterListModel {
+    
+    
+}
