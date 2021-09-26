9585f77ecd316a747af2730b3a37181c9dbc738d
Rakuyo <rakuyo.mo@gmail.com>
2021-09-26 15:59:44 +0800

    Create FilterDoneButtonView

---
+++ ColaCup/ColaCup/Core/Filter/FilterDoneButtonView.swift
@@ -0,0 +1,15 @@
+//
+//  FilterDoneButtonView.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2021/9/26.
+//  Copyright © 2021 Rakuyo. All rights reserved.
+//
+
+import UIKit
+
+/// Filter page, done button at the bottom.
+open class FilterDoneButtonView: UIView {
+    
+    
+}
