879e4f72314ae3e0a9dc2f7bf87171c2b9bf122f
Rakuyo <rakuyo.mo@gmail.com>
2021-09-26 13:37:39 +0800

    Create FilterModel

---
+++ ColaCup/ColaCup/Core/Filter/Model/FilterModel.swift
@@ -0,0 +1,23 @@
+//
+//  FilterModel.swift
+//  ColaCup
+//
+//  Created by Rakuyo on 2021/9/26.
+//  Copyright © 2021 Rakuyo. All rights reserved.
+//
+
+import Foundation
+
+import RaLog
+
+/// The model used to record the currently selected filter criteria.
+public struct FilterModel {
+    /// The currently selected time range.
+    public var selectedTimeRange: FilterTimeRange = .currentPage
+    
+    /// The set of currently selected flags.
+    public var selectedFlags = Set<Log.Flag>()
+    
+    /// The set of currently selected modules.
+    public var selectedModules = Set<String>()
+}
