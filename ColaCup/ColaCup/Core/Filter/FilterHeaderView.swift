2769153db973f9bb82e16b5c54c8ac49a48a38fd
Rakuyo <rakuyo.mo@gmail.com>
2021-09-27 13:12:42 +0800

    update

--- ColaCup/ColaCup/Core/Filter/FilterHeaderView.swift
+++ ColaCup/ColaCup/Core/Filter/FilterHeaderView.swift
@@ -25,7 +25,7 @@ public class FilterHeaderView: UICollectionReusableView {
         
         label.textAlignment = .left
         label.lineBreakMode = .byTruncatingTail
-        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.font = UIFont.preferredFont(forTextStyle: .title2)
         
         if #available(iOS 13.0, *) {
             label.textColor = .label
