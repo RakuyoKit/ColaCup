112c46b4fef791ed39d67c8e0e7b1f01083df576
Rakuyo <rakuyo.mo@gmail.com>
2021-09-29 09:45:35 +0800

    Put the ColaCupControllerDelegate into a separate file.

--- ColaCup/ColaCup/Core/ColaCup/ColaCupController.swift
+++ ColaCup/ColaCup/Core/ColaCup/ColaCupController.swift
@@ -10,34 +10,6 @@ import UIKit
 
 import RaLog
 
-public protocol ColaCupControllerDelegate: NSObjectProtocol {
-    ///  Whether to use the built-in `appear` and `disappear` methods to filter which logs belong to the current page.
-    ///
-    /// The default is `false`. if you use both logging methods, please implement this method and return `true` to improve the accuracy of the filtering results.
-    ///
-    /// Also, you need to make sure that the class name and the file name of the controller are the same.
-    /// For example, if the controller class name is "ViewController",
-    /// then the file name should be "ViewController.swift".
-    /// Otherwise it will also cause filtering errors.
-    ///
-    /// - Parameter controller: ColaCupController
-    func usedJumpFlagToFilterCurrentPageOfColaCup(_ controller: ColaCupController) -> Bool
-    
-    /// Needs to return the name of the file where the user was viewing the controller before entering the cup.
-    ///
-    /// This method will be used to implement the "Find the log of the current page" function.
-    /// If you do not implement this method, then you will not be able to use this function.
-    ///
-    /// - Parameter controller: ColaCupController
-    func nameOfFileBeforeEnterColaCup(_ controller: ColaCupController) -> String
-}
-
-public extension ColaCupControllerDelegate {
-    func usedJumpFlagToFilterCurrentPageOfColaCup(_ controller: ColaCupController) -> Bool {
-        return false
-    }
-}
-
 /// A controller for viewing logs
 open class ColaCupController: BaseLogViewController {
     /// Use the log manager to initialize the controller.
